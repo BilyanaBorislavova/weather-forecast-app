dc91a1268015d47b6e85405d500e19f5c7f00171
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-05-23 16:58:15 +0300

    Update weather-forecast-api-service.ts

--- src/services/weather-forecast-api-service.ts
+++ src/services/weather-forecast-api-service.ts
@@ -3,12 +3,12 @@ class WeatherForecastAPIService {
     weatherForecastApiKey: string;
 
     constructor() {
-        this.weatherForecastApi = 'https://api.openweathermap.org/data/2.5/weather';
+        this.weatherForecastApi = 'https://api.openweathermap.org/data/2.5/onecall';
         this.weatherForecastApiKey = '4448b0a0fb2bb13aa9d24de7dd358fc3';
     }
 
     getWeatherForecastByLatAndLon = (lat: string, lon: string) =>
-        `${this.weatherForecastApi}?lat=${lat}&lon=${lon}&appid=${this.weatherForecastApiKey}`;
+        `${this.weatherForecastApi}?lat=${lat}&lon=${lon}&units=metric&appid=${this.weatherForecastApiKey}`;
     
     getWeatherForecastByCityName = (cityName: string) => 
         `${this.weatherForecastApi}?q=${cityName}&appid=${this.weatherForecastApiKey};`
