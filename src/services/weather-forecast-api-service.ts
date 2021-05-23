b4c178968caf0819a128b53478355dc36df410c1
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-05-23 12:05:16 +0300

    Update weather-forecast-api-service.ts

--- src/services/weather-forecast-api-service.ts
+++ src/services/weather-forecast-api-service.ts
@@ -1,4 +1,4 @@
-class WeatherForecastService {
+class WeatherForecastAPIService {
     weatherForecastApi: string;
     weatherForecastApiKey: string;
 
@@ -14,4 +14,4 @@ class WeatherForecastService {
         `${this.weatherForecastApi}?q=${cityName}&appid=${this.weatherForecastApiKey};`
 }
 
-export default WeatherForecastService;
+export default WeatherForecastAPIService;
