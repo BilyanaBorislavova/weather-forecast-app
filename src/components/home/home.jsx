983e1528718e8899c4f2b95beeaba2f3520b19aa
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-05-23 17:21:40 +0300

    Update home.jsx

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,10 +1,17 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
+import useFetch from 'use-http';
 import useGeoLocation from '../../hooks/use-geo-location';
+import useWeatherForecastApi from '../../hooks/use-weather-forecast-api';
+import WeatherForecastAPIService from '../../services/weather-forecast-api-service';
 
 const Home = () => {
+    const weatherApiService = new WeatherForecastAPIService();
     const { isLoading, coordinates, hasError, error } = useGeoLocation();
     const { latitude, longitude } = coordinates;
     const { code, message } = error;
+
+    const weatherData = useWeatherForecastApi(weatherApiService, [latitude, longitude]);
+    console.log(weatherData)
 
     if (isLoading) {
         return <h1>Page is Loading!</h1>
@@ -15,7 +22,11 @@ const Home = () => {
     }
 
     return (
-        <h1>{latitude}</h1>
+        <section className="home">
+            <h1 className="home-title">
+                Your current location is
+            </h1>
+        </section>
     );
 };
 
