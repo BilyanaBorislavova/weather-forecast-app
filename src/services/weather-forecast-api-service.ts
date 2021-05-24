b837d233b7a9ef10db1e9552404508d567d1a124
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-05-24 16:30:10 +0300

    Used different api for finding weather forecast based on city name

--- src/services/weather-forecast-api-service.ts
+++ src/services/weather-forecast-api-service.ts
@@ -3,15 +3,15 @@ class WeatherForecastAPIService {
     weatherForecastApiKey: string;
 
     constructor() {
-        this.weatherForecastApi = 'https://api.openweathermap.org/data/2.5/onecall';
+        this.weatherForecastApi = 'https://api.openweathermap.org/data/2.5/';
         this.weatherForecastApiKey = '4448b0a0fb2bb13aa9d24de7dd358fc3';
     }
 
     getWeatherForecastByLatAndLon = (lat: string, lon: string) =>
-        `${this.weatherForecastApi}?lat=${lat}&lon=${lon}&units=metric&appid=${this.weatherForecastApiKey}`;
+        `${this.weatherForecastApi}onecall?lat=${lat}&lon=${lon}&units=metric&appid=${this.weatherForecastApiKey}`;
     
-    getWeatherForecastByCityName = (cityName: string) => 
-        `${this.weatherForecastApi}?q=${cityName}&appid=${this.weatherForecastApiKey};`
+    getWeatherForecastByCityName = (cityName: string) =>
+        `${this.weatherForecastApi}forecast?q=${cityName}&appid=${this.weatherForecastApiKey}`;
 }
 
 export default WeatherForecastAPIService;
