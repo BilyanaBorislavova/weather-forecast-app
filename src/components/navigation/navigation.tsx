e1fc6e20b938e464f5ef0d2538563666f67ee8a5
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-05-24 13:14:38 +0300

    Used <SearchCity /> component in navigation

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,12 +1,11 @@
+import './navigation.scss';
+
 import * as React from 'react';
-import NavigationSearchField from './navigation-search-field';
+import SearchCity from '../misc/search-city';
 
 const Navigation = () => (
   <nav className="navigation">
-    <h3 className="navigation-title">
-      Weather Forecast App
-    </h3>
-    <NavigationSearchField />
+    <SearchCity />
   </nav>
 );
 
